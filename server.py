e943f9535fbd37aee547aae1246ba6ccd64a4358
lolekgk <karol.gajda97@gmail.com>
2022-04-12 09:01:14 +0200

    Add render template

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return 'This is the main page!'
+    return render_template('index.html')
 
 
 @app.route('/note', methods=['GET', 'POST'])
 def note():
     return 'This is note page!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
