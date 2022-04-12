4434ae1387f8947bc34eb5d7a6c570018cf6d939
lolekgk <karol.gajda97@gmail.com>
2022-04-12 10:51:17 +0200

    Add display saved notes and edits count

--- server.py
+++ server.py
@@ -2,14 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+saved_data = {}
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    notes = None
+    if 'note' in saved_data:
+        notes = saved_data['note']
+    edits = saved_data.setdefault('edit_count', 0)
+    return render_template('index.html', notes=notes, edit_count=edits)
 
 
 @app.route('/note', methods=['GET', 'POST'])
 def note():
+    if request.method == 'POST':
+        saved_data['note'] = request.form
+        saved_data['edit_count'] = saved_data.setdefault('edit_count', 0) + 1
+        return redirect('/')
     return render_template('note.html')
 
 
